808ead035da87dfad12fd0146615efb27d07e79f
ashikur-rahman-asif <ashikurrahmanasif247@yahoo.com>
2026-08-11 18:47:14 +0600

    add logo component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Container from "@/components/Container";
+import Logo from "@/components/icons/logo";
 
 export default function Home() {
   return (
     <Container>
-      <div>
+      <div className="flex items-center justify-between">
         <h1 className="text-brand ">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint,
           tempora?
         </h1>
+        <Logo />
       </div>
     </Container>
   );
