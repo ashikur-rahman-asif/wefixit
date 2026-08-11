7bfc58b1dac1af3fe98a8e91abe676c468aabe89
ashikur-rahman-asif <ashikurrahmanasif247@yahoo.com>
2026-08-11 19:06:00 +0600

    new SVG icon components

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -9,8 +9,6 @@ const Logo = (props: SVGProps<SVGSVGElement>) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 104 24"
-      width={104}
-      height={24}
       fill="none"
       {...props}
     >
